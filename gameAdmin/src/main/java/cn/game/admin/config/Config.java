041b6e62220b39b9f1437b01fb836b6c16be2ead
tangjun <tang_jun2006@163.com>
2017-07-31 18:05:53 +0800

    添加配置

--- gameAdmin/src/main/java/cn/game/admin/config/Config.java
+++ gameAdmin/src/main/java/cn/game/admin/config/Config.java
@@ -1,6 +1,5 @@
 package cn.game.admin.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,39 +10,14 @@ import org.springframework.context.annotation.Configuration;
 public class Config {
 
 	public interface Values {
-		String getStaticUrlPrefix();
-
-		String getUploadedImagesDir();
-
-		String getUploadedImagesUrlPrefix();
+	
 	}
 
-	@Value("${admin.static.url-prefix}")
-	private String staticUrlPrefix;
-
-	@Value("${admin.uploaded.images.dir}")
-	private String uploadedImagesDir;
-
-	@Value("${admin.uploaded.images.url-prefix}")
-	private String uploadedImagesUrlPrefix;
-
+	
 	@Bean(name = "values")
 	public Values values() {
 		return new Values() {
-			@Override
-			public String getStaticUrlPrefix() {
-				return staticUrlPrefix;
-			}
-
-			@Override
-			public String getUploadedImagesDir() {
-				return uploadedImagesDir;
-			}
-
-			@Override
-			public String getUploadedImagesUrlPrefix() {
-				return uploadedImagesUrlPrefix;
-			}
+		
 		};
 	}
 

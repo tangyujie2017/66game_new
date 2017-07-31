94f2d844ca62b3084725796bc3b6c4e81f162276
tangjun <tang_jun2006@163.com>
2017-07-31 16:27:50 +0800

    删除无用类

--- gameAdmin/src/main/java/cn/game/admin/AdminApplication.java
+++ gameAdmin/src/main/java/cn/game/admin/AdminApplication.java
@@ -1,14 +1,18 @@
 package cn.game.admin;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-
 import cn.game.core.Core;
 @Import(Core.class)
 @SpringBootApplication
 public class AdminApplication {
+	private static Logger logger = LogManager.getLogger(AdminApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("游戏后台开始启动");
 		SpringApplication.run(AdminApplication.class, args);
 	}
 }
--- gameAdmin/src/main/java/cn/game/admin/config/PasswordEncoderBean.java
+++ gameAdmin/src/main/java/cn/game/admin/config/PasswordEncoderBean.java
@@ -1,4 +1,4 @@
-package cn.game.admin;
+package cn.game.admin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

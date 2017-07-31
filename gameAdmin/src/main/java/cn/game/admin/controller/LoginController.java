b9d6985052214eb2b27abd88d53c8d00979bd345
tangjun <tang_jun2006@163.com>
2017-07-31 16:33:51 +0800

    删除无用文件

---
+++ gameAdmin/src/main/java/cn/game/admin/controller/LoginController.java
@@ -0,0 +1,12 @@
+package cn.game.admin.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class LoginController {
+	private static Logger logger = LogManager.getLogger(LoginController.class);
+
+}
